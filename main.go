7f758eed56cbe9a57ae4f7059125fd87db385b53
rbenjami <benjamin.rouannes@ynov.com>
2025-03-25 10:07:25 +0100

    localhost 5555

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ func main() {
 	fs := http.FileServer(http.Dir("."))
 	http.Handle("/", fs)
 
-	fmt.Println("Starting server at http://localhost:8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	fmt.Println("Starting server at http://localhost:5555")
+	if err := http.ListenAndServe(":5555", nil); err != nil {
 		fmt.Println("Error starting server:", err)
 	}
 }
